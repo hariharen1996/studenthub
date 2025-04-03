9dfcfbb14510015d731236e3037373ea06bdcef8
hariharen96 <hariharen1996@gmail.com>
2025-04-03 17:02:30 +0530

    feat:configured spring beans with datasource and jdbctemplate in beans.xml
    feat:setup and closed spring applicationcontext in app.java

--- src/main/java/com/studenthub/App.java
+++ src/main/java/com/studenthub/App.java
@@ -1,13 +1,19 @@
 package com.studenthub;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.studenthub.dto.StudentDto;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println("studenthub");
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println("application context initialized");
         StudentDto dto = new StudentDto(1,"hari","chn",7.89,001,"CSE");
         System.out.println(dto);
+        ((ClassPathXmlApplicationContext)context).close();
+        System.out.println("application context closed");
     }
 }
