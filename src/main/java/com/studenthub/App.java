3a907509934d90bb29cb2aba369f59da251cdd62
hariharen96 <hariharen1996@gmail.com>
2025-04-07 10:05:40 +0530

    refactor: modified StudentDaoHelper class
    feat:tested all dao methods in App.java

--- src/main/java/com/studenthub/App.java
+++ src/main/java/com/studenthub/App.java
@@ -1,5 +1,7 @@
 package com.studenthub;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,24 +15,80 @@ import com.studenthub.dto.StudentDto;
 public class App {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        System.out.println("annotationconfig initialized");
+        System.out.println("AnnotationConfig initialized");
 
+        System.out.println("Initialized helper and setup table");
         StudentDaoHelper studentDaoHelper = context.getBean("studentDaoHelper", StudentDaoHelper.class);
         studentDaoHelper.setupStudentTable();
 
         StudentDaoImpl studentDaoImpl = context.getBean("studentDaoImpl", StudentDaoImpl.class);
-        List<StudentDto> students = studentDaoImpl.findAllStudents();
-        System.out.println(students);
 
-        System.out.println("retrieve student by name");
-        List<StudentDto> allstudents = studentDaoImpl.findStudentByName("sathya");
-        System.out.println(allstudents);
+        System.out.println("*****findAllStudents()*****");
+        List<StudentDto> allStudents = studentDaoImpl.findAllStudents();
+        System.out.println("All students: " + allStudents);
 
-        System.out.println("group student by address");
-        Map<String, List<String>> groupStudentByAddress = studentDaoImpl.groupStudentByAddress();
-        System.out.println(groupStudentByAddress);
+        System.out.println("*****findStudentByRollNo()*****");
+        StudentDto studentByRollNo = studentDaoImpl.findStudentByRollNo(1);
+        System.out.println("Student with rollno 1: " + studentByRollNo);
+
+        System.out.println("*****findStudentByName()*****");
+        List<StudentDto> studentsByName = studentDaoImpl.findStudentByName("renu");
+        System.out.println("Students named 'renu': " + studentsByName);
+
+        System.out.println("*****groupStudentByAddress()*****");
+        Map<String, List<String>> studentsByAddress = studentDaoImpl.groupStudentByAddress();
+        System.out.println("Students grouped by address: " + studentsByAddress);
+
+        System.out.println("*****insert() single student*****");
+        StudentDto newStudent = new StudentDto("sathya", "chennai", BigDecimal.valueOf(9.2), 4, "IT");
+        studentDaoImpl.insert(newStudent);
+        System.out.println("Inserted new student. All students now: " + studentDaoImpl.findAllStudents());
+
+        System.out.println("*****insert() multiple students*****");
+        List<StudentDto> newStudents = new ArrayList<>();
+        newStudents.add(new StudentDto("kumar", "delhi", BigDecimal.valueOf(7.8), 5, "ECE"));
+        newStudents.add(new StudentDto("priya", "mumbai", BigDecimal.valueOf(8.3), 6, "EEE"));
+        studentDaoImpl.insert(newStudents);
+        System.out.println("Inserted multiple students. All students now: " + studentDaoImpl.findAllStudents());
+
+        System.out.println("*****updateStudent()*****");
+        StudentDto updateStudent = new StudentDto();
+        updateStudent.setRollno(1);
+        updateStudent.setAddress("new hyderabad");
+        int updatedRows = studentDaoImpl.updateStudent(updateStudent);
+        System.out.println("Updated " + updatedRows + " rows. Student with rollno 1 now: " + 
+        studentDaoImpl.findStudentByRollNo(1));
+
+        System.out.println("*****updateStudent() multiple*****");
+        List<StudentDto> studentsToUpdate = new ArrayList<>();
+        StudentDto update1 = new StudentDto();
+        update1.setRollno(2);
+        update1.setAddress("new cmb");
+        studentsToUpdate.add(update1);
+        
+        StudentDto update2 = new StudentDto();
+        update2.setRollno(3);
+        update2.setAddress("new blr");
+        studentsToUpdate.add(update2);
+        
+        studentDaoImpl.updateStudent(studentsToUpdate);
+        System.out.println("Batch update completed. Students now: " + studentDaoImpl.findAllStudents());
+
+        System.out.println("*****deleteRecordByRollNo()*****");
+        boolean deleted = studentDaoImpl.deleteRecordByRollNo(6);
+        System.out.println("Deleted student with rollno 6: " + deleted);
+        System.out.println("All students after deletion: " + studentDaoImpl.findAllStudents());
+
+        System.out.println("*****deleteRecordByNameOrAddress()*****");
+        int deletedCount = studentDaoImpl.deleteRecordByNameOrAddress("kumar", "new hyderabad");
+        System.out.println("Deleted " + deletedCount + " students");
+        System.out.println("All students after deletion: " + studentDaoImpl.findAllStudents());
+
+        System.out.println("*****cleanUp()*****");
+        studentDaoImpl.cleanUp();
+        System.out.println("Table cleaned. All students now: " + studentDaoImpl.findAllStudents());
 
         context.close();
-        System.out.println("annotation config closed");
+        System.out.println("AnnotationConfig closed");
     }
 }
--- src/main/java/com/studenthub/dao/StudentDaoHelper.java
+++ src/main/java/com/studenthub/dao/StudentDaoHelper.java
@@ -2,6 +2,7 @@ package com.studenthub.dao;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,25 +12,51 @@ import com.studenthub.dto.StudentDto;
 @Component
 public class StudentDaoHelper {
 
-    private StudentDao studentDao;
+    private StudentDaoImpl studentDao;
 
     @Autowired
-    public void setStudentDao(StudentDao studentDao){
+    public void setStudentDao(StudentDaoImpl studentDao) {
         this.studentDao = studentDao;
     }
 
-    public void setupStudentTable(){
+    public void setupStudentTable() {
+        studentDao.cleanUp();
+        batchInsertStudents();        
+        updateStudentRecords();
+    }
 
-        StudentDto renu = new StudentDto("renu","hyd",BigDecimal.valueOf(8.50),001,"IT");
-        StudentDto menu = new StudentDto("menu","cmb",BigDecimal.valueOf(5.00),002,"CSE");
-        StudentDto banu = new StudentDto("banu","blro",BigDecimal.valueOf(6.80),003,"Civil");
-       
-        ArrayList<StudentDto> students = new ArrayList<>();
-
-        students.add(renu);
-        students.add(menu);
-        students.add(banu);
-    
+   private void batchInsertStudents() {
+        List<StudentDto> students = new ArrayList<>();
+        
+        students.add(new StudentDto("renu", "hyd", BigDecimal.valueOf(8.50), 1, "IT"));
+        students.add(new StudentDto("menu", "cmb", BigDecimal.valueOf(5.00), 2, "CSE"));
+        students.add(new StudentDto("banu", "blro", BigDecimal.valueOf(6.80), 3, "Civil"));
+        students.add(new StudentDto("sathya", "chennai", BigDecimal.valueOf(9.20), 4, "IT"));
+        students.add(new StudentDto("kumar", "delhi", BigDecimal.valueOf(7.80), 5, "ECE"));
         studentDao.insert(students);
+        System.out.println("Inserted " + students.size() + " student records");
+    }
+
+    private void updateStudentRecords() {
+        StudentDto updatedStudent = new StudentDto();
+        updatedStudent.setRollno(1);
+        updatedStudent.setAddress("new hyderabad");
+        studentDao.updateStudent(updatedStudent);
+        System.out.println("Updated address for rollno 1");
+
+       List<StudentDto> studentsToUpdate = new ArrayList<>();
+        
+        StudentDto update1 = new StudentDto();
+        update1.setRollno(2);
+        update1.setAddress("new cmb");
+        studentsToUpdate.add(update1);
+        
+        StudentDto update2 = new StudentDto();
+        update2.setRollno(3);
+        update2.setAddress("new blr");
+        studentsToUpdate.add(update2);
+        
+        studentDao.updateStudent(studentsToUpdate);
+        System.out.println("Performed batch update on " + studentsToUpdate.size() + " records");
     }
 }
